5240c86c0bfa22718a7ad4a91da1dadf31fb37fd
Glenn Gillen <me@glenngillen.com>
2014-12-05 14:40:05 +1100

    Gracefully handle if Rails is missing.

--- lib/refills/import_generator.rb
+++ lib/refills/import_generator.rb
@@ -1,50 +1,54 @@
-require 'rails/generators'
+begin
+  require 'rails/generators'
 
-module Refills
-  class ImportGenerator < Rails::Generators::Base
-    desc 'Copy refills'
-    source_root File.expand_path("../../../source", __FILE__)
-    argument :snippet, type: :string, required: true
+  module Refills
+    class ImportGenerator < Rails::Generators::Base
+      desc 'Copy refills'
+      source_root File.expand_path("../../../source", __FILE__)
+      argument :snippet, type: :string, required: true
 
-    def copy_html
-      copy_file_if_exists(
-        partial_name,
-        File.join('app', 'views', 'refills', partial_name),
-      )
-    end
+      def copy_html
+        copy_file_if_exists(
+          partial_name,
+          File.join('app', 'views', 'refills', partial_name),
+        )
+      end
 
-    def copy_styles
-      copy_file_if_exists(
-        File.join('stylesheets', 'refills', stylesheet_name),
-        File.join('app', 'assets', 'stylesheets', 'refills', stylesheet_name),
-      )
-    end
+      def copy_styles
+        copy_file_if_exists(
+          File.join('stylesheets', 'refills', stylesheet_name),
+          File.join('app', 'assets', 'stylesheets', 'refills', stylesheet_name),
+        )
+      end
 
-    def copy_javascripts
-      copy_file_if_exists(
-        File.join('javascripts', 'refills', javascript_name),
-        File.join('app', 'assets', 'javascripts', 'refills', javascript_name),
-      )
-    end
+      def copy_javascripts
+        copy_file_if_exists(
+          File.join('javascripts', 'refills', javascript_name),
+          File.join('app', 'assets', 'javascripts', 'refills', javascript_name),
+        )
+      end
 
-    private
+      private
 
-    def copy_file_if_exists(source, destination)
-      if File.exists?(File.join(self.class.source_root, source))
-        copy_file source, destination
+      def copy_file_if_exists(source, destination)
+        if File.exists?(File.join(self.class.source_root, source))
+          copy_file source, destination
+        end
+      end
+
+      def partial_name
+        "_#{snippet.underscore}.html.erb"
+      end
+
+      def stylesheet_name
+        "_#{snippet.dasherize}.scss"
+      end
+
+      def javascript_name
+        "#{snippet.underscore}.js"
       end
     end
-
-    def partial_name
-      "_#{snippet.underscore}.html.erb"
-    end
-
-    def stylesheet_name
-      "_#{snippet.dasherize}.scss"
-    end
-
-    def javascript_name
-      "#{snippet.underscore}.js"
-    end
   end
+rescue LoadError
+  puts "Rails couldn't be loaded, skipping"
 end
--- lib/refills/list_generator.rb
+++ lib/refills/list_generator.rb
@@ -1,22 +1,26 @@
-require 'rails/generators'
+begin
+  require 'rails/generators'
 
-module Refills
-  class ListGenerator < Rails::Generators::Base
-    desc 'List refills'
+  module Refills
+    class ListGenerator < Rails::Generators::Base
+      desc 'List refills'
 
-    def list
-      puts 'Available Refills'
-      puts '================='
-      stylesheets.each do |file_name|
-        puts '- ' + file_name.gsub(/_|\.scss/, '')
+      def list
+        puts 'Available Refills'
+        puts '================='
+        stylesheets.each do |file_name|
+          puts '- ' + file_name.gsub(/_|\.scss/, '')
+        end
+      end
+
+      private
+      def stylesheets
+        refills_dir = File.expand_path('../../../source/stylesheets/refills', __FILE__)
+        stylesheets = Dir.entries(refills_dir)
+        stylesheets.reject {|f| f == '.' || f == '..' }
       end
     end
-
-    private
-    def stylesheets
-      refills_dir = File.expand_path('../../../source/stylesheets/refills', __FILE__)
-      stylesheets = Dir.entries(refills_dir)
-      stylesheets.reject {|f| f == '.' || f == '..' }
-    end
   end
+rescue LoadError
+  puts "Rails couldn't be loaded, skipping"
 end
